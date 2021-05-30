03a2ab783a41c04d428d45cb45efc87da0316c95
Hilary Osborne <h.t.osborne@gmail.com>
2021-05-30 03:51:25 -0700

    feat(*): added webpack dev server

--- packages/bitsy-ui-core/webpack/webpack.config.dev.ui.js
+++ packages/bitsy-ui-core/webpack/webpack.config.dev.ui.js
@@ -59,34 +59,6 @@ module.exports = {
     compress: true,
     port: 9087,
   },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          parse: {
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            comparisons: false,
-            inline: 2,
-          },
-          mangle: {
-            safari10: true,
-          },
-          output: {
-            ecma: 5,
-            comments: false,
-            ascii_only: true,
-          },
-        },
-        parallel: true,
-        cache: false,
-        sourceMap: false,
-      }),
-    ],
-  },
   module: {
     strictExportPresence: true,
     rules: [
